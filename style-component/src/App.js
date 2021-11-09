3101c023e32c15314bd671720d58763cc3e3a4cf
MangoSteen0903 <sjlee990129@gmail.com>
2021-11-09 17:06:42 +0900

    Styled component Done ✅

--- style-component/src/App.js
+++ style-component/src/App.js
@@ -1,28 +1,19 @@
 import styled from "styled-components";
 const Father = styled.div`
   display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
-
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.span`
-  color: white;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Title>Hello</Title>
     </Father>
   );
 }
