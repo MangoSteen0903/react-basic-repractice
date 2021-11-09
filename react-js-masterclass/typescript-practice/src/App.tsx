b41b3e05042892727f178cbe2b2780665f313ab7
MangoSteen0903 <sjlee990129@gmail.com>
2021-11-09 19:08:15 +0900

    Typescript Practice Done ✅

--- react-js-masterclass/typescript-practice/src/App.tsx
+++ react-js-masterclass/typescript-practice/src/App.tsx
@@ -1,10 +1,17 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>Protected</H1>
+    </Container>
   );
 }
 export default App;
